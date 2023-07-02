9d3a0f1f4747abaddd3c39b921e436917787922d
PRINCE SAHU <91106662+Prs0307@users.noreply.github.com>
2023-07-02 09:23:32 +0530

    signup added

---
+++ src/app/server/server.ts
@@ -0,0 +1,9 @@
+export interface Server {
+    id : string;
+    organizationName : string;
+    place : string;
+    mobile : number;
+    desc : string;
+    food_img : undefined;
+    booked : boolean;
+}
